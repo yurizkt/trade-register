696144feb8ccbac53bd5ae44dc8694f21f8db19c
Yuri Zicatti <zicattiy@sao-mac-195-nc.local>
2018-08-22 13:13:22 -0300

    Adicionando mensagem quando inserir dados, somar o total dos dados e melhorando o código de negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,28 +2,43 @@ class NegociacaoController {
 
 	constructor(){
 
-		let $ = document.querySelector.bind(document);
-		this._inputData = $('#data');
-		this._inputQuantidade = $('#quantidade');
-		this._inputValor = $('#valor');
+		let $ = document.querySelector.bind(document)
+		this._inputData = $('#data')
+		this._inputQuantidade = $('#quantidade')
+		this._inputValor = $('#valor')
+		this._listaNegociacoes = new ListaNegociacoes()
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'))
+		this._negociacoesView.update(this._listaNegociacoes)
+
+		this._mensagem = new Mensagem()
+		this._mensagemView = new MensagemView($('#mensagemView'))
+		this._mensagemView.update(this._mensagem)
 	}
 
 	adiciona(event){
-		event.preventDefault();
+		event.preventDefault()
 
-		let data = new Date(...
-			this._inputData
-				.value
-				.split('-')
-				.map((item, indice) => item - indice % 2)
-		);
+		this._listaNegociacoes.adiciona(this._criaNegociacao())
+		this._negociacoesView.update(this._listaNegociacoes)
+		
+		this._mensagem.texto = 'Negociação adicionada com sucesso!'
+		this._mensagemView.update(this._mensagem)
 
-		let negociacao = new Negociacao(
-			data,
+		this._limpaFormulario()
+	}
+
+	_criaNegociacao(){
+		return new Negociacao(
+			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
-			this._inputValor.value
-		);
+			this._inputValor.value)
+	}
 
-		console.log(negociacao);
+	_limpaFormulario(){
+		this._inputData.value = ''
+		this._inputQuantidade.value = 1
+		this._inputValor.value = 0.0
+		this._inputData.focus()
 	}
 }
